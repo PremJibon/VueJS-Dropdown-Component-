ae45fd6c119738d12fb67e7d0f2f4bd99d447130
PremJibon <126474324+PremJibon@users.noreply.github.com>
2023-10-29 14:45:54 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Navbar from './components/Navbar';
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <header>
+      <Navbar />
+    </header>
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'montserrat', sans-serif;
+}
+header {
+  width: 100vh;
+  background-color: #222;
+  padding: 25px;
+}
+</style>
